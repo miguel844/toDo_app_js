4881f590c482322132ae1b4009a605c5eb2296ac
miguel844 <miguel844@gmail.com>
2026-06-30 00:06:53 +0200

    add id and text task

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,16 @@ const taskInput = document.getElementById("taskInput");
 const addTaskButton = document.getElementById("addTaskButton");
 const taskList = document.getElementById("taskList");
 let tasks = [];
+let indice = 0;
 addTaskButton.addEventListener("click", function () {
     if (taskInput.value.trim().length === 0) return;
-    const newTask = taskInput.value.trim();
+    const newTask = {
+        id: Date.now(),
+        text: taskInput.value.trim(),
+        done: false
+    };
     tasks.push(newTask);
-
+    console.log(newTask);
     renderTask(tasks);
 
     taskInput.value = "";
@@ -15,12 +20,28 @@ addTaskButton.addEventListener("click", function () {
 
 
 function renderTask(elementos) {
-    taskList.innerHTML = ""; // limpiar lista
+    taskList.innerHTML = "";
 
     for (let i = 0; i < elementos.length; i++) {
         const taskItem = document.createElement("li");
-        taskItem.textContent = elementos[i].trim();
+        const deleteItem = document.createElement("button");
+
+        taskItem.textContent = elementos[i].text;
+        taskItem.className = "mt-1";
+
+        deleteItem.textContent = "Borrar";
+        deleteItem.className = "px-8 py-1 bg-[#ff5050] rounded-md cursor-pointer ml-4";
+
+        deleteItem.addEventListener("click", function () {
+            tasks = tasks.filter(task => task.id !== elementos[i].id);
+            renderTask(tasks);
+        });
+
+        taskItem.appendChild(deleteItem);
         taskList.appendChild(taskItem);
     }
+
+    taskInput.value = "";
+    taskInput.focus();
 }
 
