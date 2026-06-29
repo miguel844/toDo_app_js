0da2f136fc5bb311da5c9ce3b9129c85713d0311
miguel844 <miguel844@gmail.com>
2026-06-29 23:22:21 +0200

    add render task

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTaskButton");
+const taskList = document.getElementById("taskList");
+let tasks = [];
+addTaskButton.addEventListener("click", function () {
+    if (taskInput.value.trim().length === 0) return;
+    const newTask = taskInput.value.trim();
+    tasks.push(newTask);
+
+    renderTask(tasks);
+
+    taskInput.value = "";
+    taskInput.focus();
+});
+
+
+function renderTask(elementos) {
+    taskList.innerHTML = ""; // limpiar lista
+
+    for (let i = 0; i < elementos.length; i++) {
+        const taskItem = document.createElement("li");
+        taskItem.textContent = elementos[i].trim();
+        taskList.appendChild(taskItem);
+    }
+}
+
